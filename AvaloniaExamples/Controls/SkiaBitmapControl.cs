2b0e51f0008a627965bb5daa0b7cf9ffc96fac82
Juan Pablo <juanpablogc@gmail.com>
2022-04-01 15:34:30 +0100

    fixed AffectsMeasure in SkiaBitmapControl

--- AvaloniaExamples/Controls/SkiaBitmapControl.cs
+++ AvaloniaExamples/Controls/SkiaBitmapControl.cs
@@ -43,7 +43,7 @@ namespace AvaloniaExamples.Controls
         /// </summary>
         static SkiaBitmapControl()
         {
-            AffectsMeasure<SkiaBitmapCColorControl>(TransformedBoundsProperty);
+            AffectsMeasure<SkiaBitmapControl>(TransformedBoundsProperty);
         }
 
 
